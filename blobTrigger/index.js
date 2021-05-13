6b6a1b84ddc7e20fe1ade2adb7a17c7ae4c60f51
Matthew <23434496+MatthewSeelig@users.noreply.github.com>
2021-05-13 17:55:28 +0100

    better logging & error log handling

--- blobTrigger/index.js
+++ blobTrigger/index.js
@@ -11,14 +11,15 @@ const containerClient = storage.BlobServiceClient.fromConnectionString(fileStora
 module.exports.index = async function(context, inputBlob) {
 const inputBlobName = path.basename(context.bindingData.blobTrigger)
     try {
-        context.log("Instant Api blob trigger function triggered by", inputBlobName, "(filesize:", inputBlob.length, "Bytes)")
+        context.log(`Instant Api blob trigger function triggered by "${inputBlobName}" (filesize: ${inputBlob.length} Bytes)`)
 
         const guessedMimeType = mime.lookup(inputBlobName)
         if(guessedMimeType) 
-            context.log("Guessed mime type for", inputBlobName, "is", guessedMimeType)
+            context.log(`Guessed mime type for "${inputBlobName}" is ${guessedMimeType}`)
         else
             throw new Error("Mime type could not be guessed (missing or unknown suffix)")
 
+        context.log(`Sending "${inputBlobName}" to instant API`)
         const response = await upload(
             API_ENDPOINT_BASE,
             API_KEY,
@@ -28,7 +29,7 @@ const inputBlobName = path.basename(context.bindingData.blobTrigger)
         
         containerClient.getBlockBlobClient(inputBlobName).deleteIfExists()
 
-        context.log("file", inputBlobName, "successfully transformed\n")
+        context.log(`"${inputBlobName}" successfully transformed`)
 
         return {
             successBlob: response.data
@@ -37,44 +38,58 @@ const inputBlobName = path.basename(context.bindingData.blobTrigger)
     catch(err) {
         containerClient.getBlockBlobClient(inputBlobName).deleteIfExists()
 
-        context.log.error("An error has occured during the processing of", inputBlobName)
+        context.log.error(`An error has occured during the processing of "${inputBlobName}"`)
 
         if(err.response && err.response.data) {
-            context.log.error(err.response.data, "\n")
-            return {
-                errorBlob: err.response.data
+            try {
+                const parsedJsonError = JSON.parse(err.response.data)
+                context.log.error(parsedJsonError)
+                return {
+                    errorBlob: parsedJsonError
+                }
+            }
+            catch(json_err) {
+                try {
+                    const statusString = `HTTP ${err.response.status} ${err.response.statusText}`
+                    const parsedHttpData = new Buffer.from(err.response.data).toString()
+                    const httpError = {"error": {"type": statusString, "message": parsedHttpData}};
+                    context.log.error(httpError)
+                    return {
+                        errorBlob: httpError
+                    }
+                }
+                catch(parse_err) {
+                    return {
+                        errorBlob: err.response.data
+                    }
+                }
+
             }
         }
         else {
-            const errorJson = {"error": {"type": "INSTANT_API_CODE_SAMPLE_ERROR", "message": err.message}};
-            context.log.error(errorJson, "\n")
+            const sampleCodeError = {"error": {"type": "INSTANT_API_CODE_SAMPLE_ERROR", "message": err.message}};
+            context.log.error(sampleCodeError)
             return {
-                errorBlob: errorJson
+                errorBlob: sampleCodeError
             }
         }
     }
 };
 
 async function upload(apiEndpointBase, apiKey, inputBlob, mimeType) {
-    try {
-        const response = await axios({
-            method: 'POST',
-            headers: {
-                'Accept': [mimeType, 'application/json'].join(', '),
-                'Content-Type': mimeType,
-                'x-api-key': apiKey
-            },
-            responseType: 'arraybuffer',
-            url: apiEndpointBase + '/upload',
-            maxContentLength: Math.max(20000, inputBlob.length * 3),
-            maxBodyLength: Math.max(20000, inputBlob.length),
-            data: inputBlob
-        })
+    const response = await axios({
+        method: 'POST',
+        headers: {
+            'Accept': [mimeType, 'application/json'].join(', '),
+            'Content-Type': mimeType,
+            'x-api-key': apiKey
+        },
+        responseType: 'arraybuffer',
+        url: apiEndpointBase + '/upload',
+        maxContentLength: Math.max(20000, inputBlob.length * 3),
+        maxBodyLength: Math.max(20000, inputBlob.length),
+        data: inputBlob
+    })
 
-        return response
-    }
-    catch (err) {
-        if (err.response && err.response.data) err.response.data = JSON.parse(err.response.data)
-        throw err
-    }
+    return response
 }
